ef666e1c99fe1eed7fa23bf2cb8fe027dd402f05
Matthias Henning <Matze_GitHub@gmx.de>
2017-10-23 21:43:59 +0200

    Gute Nacht

--- Sudoku/Board.fs
+++ Sudoku/Board.fs
@@ -1,13 +1,12 @@
 ﻿namespace Sudoku
 
-open System.Collections.Generic
-
 open Cell
 open Digit
 
 module Board =
     type Board= {
          Cells : Map<Point, Cell>
-        }
-
-    let EmptyBoard = {Cells = AllDigits2D |> List.fold (fun acc x -> Map.add x (Cell.UnKnown Set.empty) acc) Map.empty}
+                } 
+     with 
+         static member Empty = 
+            {Cells = AllDigits2D |> List.fold (fun acc x -> Map.add x (Cell.UnKnown Set.empty) acc) Map.empty}
