fdb19a1fb472f7eb4efbed6956b40adf8a0c3f8c
TECH2GO\mhenning <matze_henning@gmx.de>
2017-10-23 17:44:21 +0200

    First commit

---
+++ Sudoku/Digit.fs
@@ -0,0 +1,25 @@
+﻿namespace Sudoku
+
+module Digit=
+  
+    type Digits =
+     | One = 1
+     | Two = 2
+     | Three = 3
+
+     | Four = 4
+     | Five = 5
+     | Six = 6
+
+     | Seven = 7
+     | Eight = 8
+     | Nine = 9
+
+    let AllDigits = [1..9] |> List.map (fun i -> enum<Digits>(i)) 
+   
+    let AllDigits2D = 
+        seq{
+          for x in AllDigits do 
+           for y in AllDigits do
+              yield x,y
+           } |> List.ofSeq
