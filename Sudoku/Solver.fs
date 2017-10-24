4f72f050e4d1909b82dd662478e26f4ef505811e
TECH2GO\mhenning <matze_henning@gmx.de>
2017-10-24 18:04:23 +0200

    Feierabend

--- Sudoku/Solver.fs
+++ Sudoku/Solver.fs
@@ -1,7 +1,12 @@
 ﻿namespace Sudoku
 
 module Solver=
+    open Digit
 
-    let Solver board=
-        board
+    let splitLine (line:string) =
+        line.Split(';') |> Seq.mapi (fun i item -> Digits.ToDigit (i + 1), Digits.ToDigit (System.Int32.Parse(item)))
+
+    let Import lines =
+        let lines = Seq.filter (fun (line:string) -> line.Contains(";") ) lines
+        Seq.head lines |> splitLine
 
---
+++ Tests/SolverTests.fs
@@ -0,0 +1,26 @@
+﻿namespace Tests
+
+open NUnit.Framework
+open System.IO
+open System.Reflection
+open Sudoku.Solver
+
+module SolverTest=
+  
+
+    [<TestFixture>]
+    type MethodTests() =
+        [<Test>]
+        member x.SolverTest()  =
+            let dir = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            let file = Path.Combine(dir, "data.txt")
+
+            let allLines =  File.ReadAllLines file |> Import
+            ()
+           
+          
+            
+
+      
+
+      
